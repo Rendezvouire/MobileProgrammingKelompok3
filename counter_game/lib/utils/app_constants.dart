b8432f4c289c8657230755cfef8456a2aae87c4a
aurelianania <nania.aurelia@gmail.com>
2026-09-12 18:46:40 +0700

    Update app_constants.dart

---
+++ counter_game/lib/utils/app_constants.dart
@@ -0,0 +1,31 @@
+class AppConstants {
+  static const String appName = 'SCORE COUNTER';
+
+  // DEFAULT GAME SETTINGS
+  // Nilai awal target poin untuk memenangkan pertandingan
+  static const int defaultMaxScore = 21;
+
+  // Nilai awal skor kedua pemain
+  static const int initialScore = 0;
+
+  // Deuce
+  static const int deucePointDifference = 2;
+
+  // Nilai minimum dan maksimum target poin
+  static const int minMaxScore = 1;
+  static const int maxMaxScore = 99;
+
+  // PLAYER
+  static const String player1Name = 'PLAYER 1';
+  static const String player2Name = 'PLAYER 2';
+
+  // PRESET GAME
+  static const int badmintonScore = 21;
+  static const int volleyballScore = 25;
+  static const int tennisScore = 11;
+
+  // GAME STATE
+  static const int noWinner = 0;
+  static const int player1 = 1;
+  static const int player2 = 2;
+}
