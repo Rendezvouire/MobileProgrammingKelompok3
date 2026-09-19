8c89e56a4b9a038d7ba1a7b43dbf187b1500da25
athaya <02athayaka@gmail.com>
2026-09-19 17:22:35 +0700

    Update hero_model.dart

--- national_heroes/lib/models/hero_model.dart
+++ national_heroes/lib/models/hero_model.dart
@@ -4,6 +4,8 @@ class HeroModel {
   final String origin;
   final String birthDeath;
   final String biography;
+  final String? subtitle;
+  final String? fullName;
 
   HeroModel({
     required this.name,
@@ -11,5 +13,7 @@ class HeroModel {
     required this.origin,
     required this.birthDeath,
     required this.biography,
+    this.subtitle,
+    this.fullName,
   });
 }
