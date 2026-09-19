b31b8d3a0ff9ce8002b61a839edde115e9b992cd
Catherine Patricia Sindunata <pxtricix.sindunata@gmail.com>
2026-09-19 10:00:27 +0700

    Update Biography Pahlawan dan Configurations

--- national_heroes/lib/data/hero_data.dart
+++ national_heroes/lib/data/hero_data.dart
@@ -2,108 +2,123 @@ import '../models/hero_model.dart';
 
 final List<HeroModel> heroes = [
   HeroModel(
-    name: 'Ahmad Yani',
+    name: 'Jenderal Ahmad Yani',
     image: 'assets/images/ahmad_yani.jpg',
     origin: 'Purworejo, Jawa Tengah',
     birthDeath: '1922 – 1965',
-    biography: 'Pahlawan Revolusi dan Menteri/Panglima Angkatan Darat.',
+    biography:
+        'Jenderal Ahmad Yani adalah perwira TNI Angkatan Darat yang pernah menjabat sebagai Menteri/Panglima Angkatan Darat. Ia memiliki peran dalam berbagai operasi militer setelah kemerdekaan Indonesia. Ahmad Yani meninggal pada 1 Oktober 1965 dalam peristiwa Gerakan 30 September.',
   ),
   HeroModel(
     name: 'Bung Tomo',
     image: 'assets/images/bung_tomo.jpg',
     origin: 'Surabaya, Jawa Timur',
     birthDeath: '1920 – 1981',
-    biography: 'Tokoh sentral pengobar semangat juang Arek-Arek Suroboyo dalam Pertempuran 10 November 1945.',
+    biography:
+        'Bung Tomo, yang bernama Sutomo, dikenal karena perannya dalam membangkitkan semangat rakyat Surabaya menjelang dan selama Pertempuran 10 November 1945. Pidato-pidatonya melalui radio menjadi salah satu simbol perjuangan rakyat Surabaya dalam mempertahankan kemerdekaan.',
   ),
   HeroModel(
     name: 'Cut Nyak Dien',
     image: 'assets/images/cut_nyak_dien.jpg',
     origin: 'Lampadang, Aceh',
     birthDeath: '1848 – 1908',
-    biography: 'Pahlawan nasional perempuan asal Aceh yang memimpin perlawanan gerilya melawan Belanda.',
+    biography:
+        'Cut Nyak Dien merupakan pejuang perempuan dari Aceh yang terlibat dalam Perang Aceh melawan Belanda. Setelah suaminya Teuku Umar gugur, ia melanjutkan perjuangan dengan memimpin perlawanan dari pedalaman Aceh. Ia kemudian ditangkap dan diasingkan ke Sumedang hingga wafat pada 6 November 1908.',
   ),
   HeroModel(
     name: 'Cut Nyak Meutia',
     image: 'assets/images/cut_nyak_meutia.jpg',
     origin: 'Keureutoe, Pirak, Aceh',
     birthDeath: '1870 – 1910',
-    biography: 'Pahlawan nasional wanita dari Aceh yang gugur dalam pertempuran melawan pasukan Belanda.',
+    biography:
+        'Cut Nyak Meutia adalah pejuang perempuan dari Aceh yang bersama suaminya terlibat dalam perlawanan terhadap Belanda. Ia melanjutkan perjuangan setelah suaminya gugur dan memimpin pasukan dalam perlawanan gerilya. Cut Nyak Meutia gugur dalam pertempuran pada 24 Oktober 1910.',
   ),
   HeroModel(
     name: 'Pangeran Diponegoro',
     image: 'assets/images/diponegoro.jpg',
     origin: 'Yogyakarta',
     birthDeath: '1785 – 1855',
-    biography: 'Pemimpin Perang Jawa (1825–1830) yang sangat menguras sumber daya dan kas tentara kolonial Belanda.',
+    biography:
+        'Pangeran Diponegoro adalah tokoh yang memimpin Perang Jawa melawan pemerintah kolonial Belanda pada 1825–1830. Perlawanan tersebut menjadi salah satu perang besar yang dihadapi Belanda di Jawa. Setelah ditangkap melalui sebuah perundingan pada 1830, Diponegoro diasingkan hingga wafat di Makassar pada 8 Januari 1855.',
   ),
   HeroModel(
-    name: 'Dr. Soetomo',
+    name: 'Dr. Sutomo',
     image: 'assets/images/dr_sutomo.jpg',
     origin: 'Nganjuk, Jawa Timur',
     birthDeath: '1888 – 1938',
-    biography: 'Salah satu pendiri organisasi Budi Utomo yang menjadi tonggak awal era Kebangkitan Nasional.',
+    biography:
+        'Dr. Sutomo adalah dokter dan tokoh pergerakan nasional yang dikenal sebagai salah satu pendiri Boedi Oetomo. Organisasi tersebut menjadi salah satu organisasi penting dalam perkembangan pergerakan nasional Indonesia. Sutomo kemudian aktif dalam berbagai kegiatan sosial dan politik hingga akhir hayatnya.',
   ),
   HeroModel(
     name: 'Tuanku Imam Bonjol',
     image: 'assets/images/imam_bonjol.jpg',
     origin: 'Bonjol, Sumatra Barat',
     birthDeath: '1772 – 1864',
-    biography: 'Pemimpin Perang Padri di Sumatra Barat dalam menentang penjajahan kolonial Belanda.',
+    biography:
+        'Tuanku Imam Bonjol adalah ulama dan pemimpin perlawanan dalam Perang Padri di Sumatera Barat. Ia memimpin perjuangan melawan Belanda dan kemudian ditangkap serta diasingkan ke wilayah Minahasa. Ia wafat di tempat pengasingannya pada 6 November 1864.',
   ),
   HeroModel(
     name: 'R.A. Kartini',
     image: 'assets/images/kartini.jpg',
     origin: 'Jepara, Jawa Tengah',
     birthDeath: '1879 – 1904',
-    biography: 'Pelopor emansipasi wanita Indonesia yang memperjuangkan kesetaraan hak serta akses pendidikan perempuan.',
+    biography:
+        'R.A. Kartini dikenal sebagai pelopor kebangkitan dan pendidikan perempuan pribumi. Ia berasal dari keluarga bangsawan Jawa dan banyak menuangkan pemikirannya mengenai pendidikan serta kedudukan perempuan melalui surat-suratnya. Gagasannya kemudian menjadi bagian penting dalam perkembangan pendidikan perempuan di Indonesia.',
   ),
   HeroModel(
     name: 'Ki Hajar Dewantara',
     image: 'assets/images/ki_hajar.jpg',
     origin: 'Yogyakarta',
     birthDeath: '1889 – 1959',
-    biography: 'Bapak Pendidikan Nasional Indonesia dan perintis lembaga pendidikan Perguruan Taman Siswa.',
+    biography:
+        'Ki Hajar Dewantara adalah tokoh pendidikan nasional dan pendiri Perguruan Taman Siswa. Ia memperjuangkan kesempatan pendidikan bagi masyarakat pribumi pada masa kolonial. Pemikirannya memberikan pengaruh besar terhadap perkembangan pendidikan Indonesia.',
   ),
   HeroModel(
     name: 'Martha Christina Tiahahu',
     image: 'assets/images/martha_tiahahu.jpg',
     origin: 'Nusa Laut, Maluku Tengah',
     birthDeath: '1800 – 1818',
-    biography: 'Pahlawan kemerdekaan termuda yang memimpin perlawanan rakyat Maluku bersama Kapitan Pattimura.',
+    biography:
+        'Martha Christina Tiahahu adalah pejuang perempuan dari Maluku yang ikut berjuang bersama Pattimura melawan Belanda pada 1817. Ia ikut dalam beberapa pertempuran sejak usia muda. Setelah ditangkap, ia meninggal di perjalanan menuju pengasingan pada 2 Januari 1818.',
   ),
   HeroModel(
     name: 'I Gusti Ngurah Rai',
     image: 'assets/images/ngurah_rai.jpg',
     origin: 'Badung, Bali',
     birthDeath: '1906 – 1946',
-    biography: 'Panglima laskar Bali yang gugur dalam Perang Puputan Margarana melawan pasukan Belanda.',
+    biography:
+        'I Gusti Ngurah Rai adalah pemimpin perjuangan bersenjata di Bali dalam mempertahankan kemerdekaan Indonesia. Ia memimpin pasukan dalam perlawanan terhadap Belanda dan gugur dalam Pertempuran Margarana pada 20 November 1946.',
   ),
   HeroModel(
     name: 'Otto Iskandardinata',
     image: 'assets/images/otto_iskandardinata.jpg',
     origin: 'Bandung, Jawa Barat',
     birthDeath: '1897 – 1945',
-    biography: 'Tokoh pergerakan nasional berjuluk "Si Jalak Harupat" dan anggota BPUPKI/PPKI.',
+    biography:
+        'Otto Iskandardinata merupakan tokoh pergerakan nasional dan salah satu anggota BPUPKI. Ia aktif dalam perjuangan politik serta organisasi pergerakan sebelum dan setelah kemerdekaan Indonesia. Ia juga dikenal dengan julukan Si Jalak Harupat.',
   ),
   HeroModel(
     name: 'Kapitan Pattimura',
     image: 'assets/images/pattimura.jpg',
     origin: 'Saparua, Maluku',
     birthDeath: '1783 – 1817',
-    biography: 'Pahlawan nasional yang memimpin rakyat Maluku merebut Benteng Duurstede dari Belanda.',
+    biography:
+        'Pattimura, atau Thomas Matulessy, adalah pemimpin perlawanan rakyat Maluku terhadap Belanda pada 1817. Ia memimpin berbagai perlawanan terhadap kekuasaan kolonial di Maluku. Pattimura akhirnya ditangkap dan dihukum mati pada 16 Desember 1817.',
   ),
   HeroModel(
-    name: 'Jenderal Soedirman',
+    name: 'Jenderal Sudirman',
     image: 'assets/images/sudirman.jpg',
     origin: 'Purbalingga, Jawa Tengah',
     birthDeath: '1916 – 1950',
-    biography: 'Panglima besar TNI pertama yang memimpin strategi perang gerilya dengan tandu.',
+    biography:
+        'Jenderal Sudirman merupakan Panglima Besar TNI pertama. Ia memimpin perjuangan mempertahankan kemerdekaan Indonesia, termasuk perang gerilya ketika Belanda melancarkan Agresi Militer II pada 1948. Meskipun mengalami masalah kesehatan, ia tetap memimpin perjuangan hingga kembali ke Yogyakarta pada 1949.',
   ),
   HeroModel(
     name: 'Sultan Hasanuddin',
     image: 'assets/images/sultan_hasannudin.jpg',
     origin: 'Gowa, Sulawesi Selatan',
     birthDeath: '1631 – 1670',
-    biography: 'Raja Gowa ke-16 berjuluk "Ayam Jantan dari Timur" atas keberaniannya melawan monopoli VOC Belanda.',
+    biography:
+        'Sultan Hasanuddin adalah Sultan Gowa yang memimpin perlawanan terhadap VOC. Ia berusaha mempertahankan kedaulatan Kerajaan Gowa dari campur tangan VOC. Karena keberaniannya melawan VOC, ia dikenal dengan julukan Ayam Jantan dari Timur.',
   ),
 ];
